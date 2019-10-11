f0621a6e539b1439ac16defc4a42d8fdfa535c1a
Ruben Bergshoeff <rubicalme@gmail.com>
2019-10-11 08:29:13 +0200

    set up basic screen flow

---
+++ RPG_Prototype/Assets/CORE/Scripts/CharacterScreenController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CharacterScreenController : MonoBehaviour {
+    [SerializeField] private PlayerCharacterController playerCharacterController = null;
+    [SerializeField] private TextMeshProUGUI provenPowerLvlField = null;
+    [SerializeField] private TextMeshProUGUI foodLvlField = null;
+
+    private void OnEnable() {
+        provenPowerLvlField.text = playerCharacterController.Data.ProvenSkillLevel.ToString();
+        foodLvlField.text = playerCharacterController.Data.CurrentFoodAmount.ToString();
+    }
+}
---
+++ RPG_Prototype/Assets/CORE/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour {
+    public PlayerCharacterData Data;
+
+    public int MaxFood = 3;
+    public int MaxLevelDeviation = 2;
+
+    public void FillFood() {
+        Data.CurrentFoodAmount = MaxFood;
+    }
+
+    public bool UseFood(int amount) {
+        if (Data.CurrentFoodAmount >= amount) {
+            Data.CurrentFoodAmount -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void SetProvenSkillLevel(int level) {
+        Data.ProvenSkillLevel = level;
+    }
+
+    public void IncreasePotentialSkillLevel(int amount) {
+        Data.PotentialSkillLevel += amount;
+        if (Data.PotentialSkillLevel - Data.ProvenSkillLevel > MaxLevelDeviation) {
+            Data.PotentialSkillLevel = Data.ProvenSkillLevel + MaxLevelDeviation;
+        }
+    }
+}
