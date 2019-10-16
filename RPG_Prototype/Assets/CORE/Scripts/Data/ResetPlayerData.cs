ad6ffbad1ae9e03fea09e4d362524c641169d9ed
Ruben Bergshoeff <rubicalme@gmail.com>
2019-10-16 08:41:01 +0200

    added reset player data button

---
+++ RPG_Prototype/Assets/CORE/Scripts/Data/ResetPlayerData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayerData : MonoBehaviour {
+    [SerializeField] private PlayerCharacterController playerCharacterController = null;
+
+    public void Reset() {
+        playerCharacterController.Data.CurrentMoodLevel = 2;
+        playerCharacterController.Data.StatPower.XP = 0;
+        playerCharacterController.Data.StatMagic.XP = 0;
+        playerCharacterController.Data.ProvenLevel = 0;
+    }
+}
