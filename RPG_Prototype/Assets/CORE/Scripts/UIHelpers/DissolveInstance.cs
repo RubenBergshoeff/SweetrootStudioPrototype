37da06db2533ab82e3b0d3a761a5a84c956d4104
Ruben Bergshoeff <rubicalme@gmail.com>
2019-11-25 08:51:05 +0100

    Final version shader

---
+++ RPG_Prototype/Assets/CORE/Scripts/UIHelpers/DissolveInstance.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveInstance : MonoBehaviour {
+    [SerializeField] private Material dissolveMaterial = null;
+    [SerializeField] private Material endMaterial = null;
+
+    private Renderer rend;
+    private float visibility = 0;
+    private bool isRunning = false;
+
+    private void OnEnable() {
+        rend = GetComponent<Renderer>();
+        StartSetup();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            isRunning = !isRunning;
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            StartSetup();
+        }
+        if (isRunning == false) { return; }
+
+        if (visibility < 1) {
+            visibility += Time.deltaTime / 2.5f;
+            visibility = Mathf.Clamp01(visibility);
+            rend.material.SetFloat("_Visibility", visibility);
+        } else if (visibility >= 1) {
+            rend.material = endMaterial;
+        }
+    }
+
+    private void StartSetup() {
+        rend.material = dissolveMaterial;
+        visibility = 0;
+        rend.material.SetFloat("_Visibility", visibility);
+    }
+}
