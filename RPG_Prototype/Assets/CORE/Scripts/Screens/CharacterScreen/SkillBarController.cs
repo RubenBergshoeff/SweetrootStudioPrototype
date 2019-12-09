ca7b97f79018522284a8278e7df168c925ef663b
Ruben Bergshoeff <rubicalme@gmail.com>
2019-12-09 15:38:33 +0100

    implemented character screen images, started working on skillbar

---
+++ RPG_Prototype/Assets/CORE/Scripts/Screens/CharacterScreen/SkillBarController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillBarController : MonoBehaviour {
+
+    [SerializeField] private BoterkroonSkills targetSkill;
+    [SerializeField] private MultiLevelSlider multiSlider = null;
+
+    private void OnEnable() {
+        List<float> normalizedResults = new List<float>();
+        foreach (var result in SaveController.Instance.GameData.BoterKroon.GetControlResultsFor(targetSkill)) {
+
+        }
+    }
+}
--- RPG_Prototype/Assets/CORE/Scripts/UIHelpers/MultiLevelSlider.cs
+++ RPG_Prototype/Assets/CORE/Scripts/UIHelpers/MultiLevelSlider.cs
@@ -1,20 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MultiLevelSlider : MonoBehaviour {
 
     [SerializeField] private List<FillSlider> sliders = new List<FillSlider>();
+    [SerializeField] private FillSlider templateSlider = null;
 
     public void UpdateValues(params float[] values) {
         if (values.Length != sliders.Count) {
-            throw new System.ArgumentException("Amount of values does not match amount of sliders");
+            UpdateSliderAmount(values.Length);
         }
 
         float startpoint = 0;
         for (int i = 0; i < values.Length; i++) {
             sliders[i].SetValues(startpoint, values[i]);
             startpoint += values[i];
+        }
+    }
+
+    private void UpdateSliderAmount(int length) {
+        while (sliders.Count > length) {
+            sliders.RemoveAt(sliders.Count - 1);
+        }
+        while (sliders.Count < length) {
+            templateSlider.gameObject.SetActive(true);
+            GameObject newSliderObject = Instantiate(templateSlider.gameObject, templateSlider.transform.parent);
+            FillSlider slider = newSliderObject.GetComponent<FillSlider>();
+            sliders.Add(slider);
+            templateSlider.gameObject.SetActive(false);
         }
     }
 }
