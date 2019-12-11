5dd02f5812ac74ce318b2a66970a8ef86f9ea7e5
Ruben Bergshoeff <rubicalme@gmail.com>
2019-12-11 16:37:03 +0100

    implemented tutorialization controller and animation

---
+++ RPG_Prototype/Assets/CORE/Scripts/Tutorialization/PointerHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PointerHelper : MonoBehaviour {
+
+    [SerializeField] private RectTransform rectTransform = null;
+
+    private Sequence tweenSequence = null;
+
+    private void OnEnable() {
+        float ypos = rectTransform.anchoredPosition.y;
+        tweenSequence = DOTween.Sequence();
+        tweenSequence.Append(rectTransform.DOAnchorPosY(ypos - 30, 0.3f).SetEase(Ease.InOutSine));
+        tweenSequence.Append(rectTransform.DOAnchorPosY(ypos, 0.3f).SetEase(Ease.InOutSine));
+        tweenSequence.SetLoops(-1);
+        tweenSequence.Play();
+    }
+
+    private void OnDisable() {
+        tweenSequence.Kill();
+    }
+}
---
+++ RPG_Prototype/Assets/CORE/Scripts/Tutorialization/TutorializationController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public enum PointDirection {
+    Up,
+    Down
+}
+
+public class TutorializationController : MonoBehaviour {
+
+    [SerializeField] private GameObject pointPrefab = null;
+    [SerializeField] private Button testButton = null;
+    [SerializeField] private Transform container = null;
+    [SerializeField] private CanvasGroup canvasGroup = null;
+
+    private GameObject pointObject = null;
+    private Transform originalParent = null;
+
+    private void Awake() {
+        canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.interactable = false;
+    }
+
+    [ContextMenu("Test Pointer")]
+    private void TestPointer() {
+        PointAtButton(testButton);
+    }
+
+    public void PointAtButton(Button button, PointDirection pointDirection = PointDirection.Down) {
+        canvasGroup.DOFade(1, 0.3f);
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.interactable = true;
+        RectTransform rectTransform = button.GetComponent<RectTransform>();
+        Vector2 pointPosition = new Vector2(0, (rectTransform.sizeDelta.y / 2 + 50) * (pointDirection == PointDirection.Down ? 1 : -1));
+        originalParent = button.transform.parent;
+        button.transform.SetParent(container);
+        pointObject = Instantiate(pointPrefab, button.transform);
+        pointObject.GetComponent<RectTransform>().anchoredPosition = pointPosition;
+        button.onClick.AddListener(RemovePointer);
+    }
+
+    private void RemovePointer() {
+        canvasGroup.DOFade(0, 0.3f);
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.interactable = false;
+        if (pointObject != null) {
+            pointObject.transform.parent.GetComponent<Button>().onClick.RemoveListener(RemovePointer);
+            pointObject.transform.parent.SetParent(originalParent);
+            Destroy(pointObject);
+        }
+    }
+}
